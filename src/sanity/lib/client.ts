b37e079e2be9f3cd12288143ceb0dce3b1fc0b6b
Wk28835 <163337656+Wk28835@users.noreply.github.com>
2025-01-03 23:09:33 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId} from '../env'
 
 const sanityClient = createClient({
-  projectId,
-  dataset,
+  projectId="z2gphn7f",
+  dataset="production",
   apiVersion,
   useCdn: true,
   token:"sk7kd1QVQPX5EwHOlmYBTxk97Ftd5F30zCH7TIog6sQr4PWm0pQ78IgiDWqU5TpHvETl9BTtQXnesrZW33M6q7aamZISIRuJDrlapPKApLhPtWV8w6DbkAR7amNTe2XrWUAFCCMcfl5n0pllIyhbAciRrNvptUz20QZvdGxykqHy642FWTFl" // Set to false if statically generating pages, using ISR or tag-based revalidation
