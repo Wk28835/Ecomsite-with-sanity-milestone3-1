4ee9f086f6e95664168234a20fe98b323a35cb87
Wk28835 <163337656+Wk28835@users.noreply.github.com>
2025-01-12 21:21:17 +0500

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import product from '../product'
 import user from '../user'
+import cart from '../cart'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product,user],
+  types: [product,user,cart],
 }
