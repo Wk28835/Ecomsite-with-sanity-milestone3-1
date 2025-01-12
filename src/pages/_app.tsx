3b55db982eb6ec5e7972299b17eea5f7260f9c6c
Wk28835 <163337656+Wk28835@users.noreply.github.com>
2025-01-12 21:25:28 +0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "./components/Navbar";  // Assuming Navbar is in the components folder
-import Footer from "./components/Footerr";  // Assuming Navbar is in the components folder
-
+ 
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen flex-grow">
       <Navbar />  {/* Render Navbar */}
       
       <Component {...pageProps} />
-      <Footer/>  {/* Render the page component */}
-    </>
+        {/* Render the page component */}
+    </div>
   );
   }
 
