78c3dd1f5148ab2288f73e2b5ec750d00eec1659
Wk28835 <163337656+Wk28835@users.noreply.github.com>
2025-01-12 21:27:04 +0500

    Update cart.tsx

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,17 +1,65 @@
 
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-const cart: React.FC = () => {
-
+interface Cart {
+  _id: string;
+  title: string;
+  price: number;
+  category: string;
+  image: string;
+  slug?: { current: string };
+}
   
+
+
+
+
+export default function cart() {
+
+  const [products,setProducts]=useState<Cart[]>([]);
  
+    useEffect(() => {
+        async function fetchProducts() {
+          try {
+            const res = await fetch("/api/cart");
+            if (!res.ok) {
+              throw new Error("Failed to fetch products");
+            }
+            const data: Cart[] = await res.json();
+            console.log("check image",data);
+            setProducts(data);
+            
+          } catch (error) {
+            console.error("Error fetching products:", error);
+          }
+        }
+        fetchProducts();
+      }, []);
+
+      const handleRemoveCart = async (cartId: string): Promise<void> => {
+        const response = await fetch("/api/cart", {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ cartId }),
+        });
+        if(response.ok){
+          setProducts(products.filter((products)=> products._id !==cartId))
+        }else{
+            console.error("Failed to Delete Cart Item!")
+        }
+      }
+
+      
+        
     return (
-    <div>
+   
         
-    <div className="lg:flex-row gap-8"
-    style={{width:"1100px",height:"632.89px",top:"40px",left:"155.5px",position:"relative"}}>
+    <div className="w-full h-full lg:flex-row gap-8 relative"
+    style={{top:"40px",left:"155.5px",position:"relative"}}>
         
-        <section className="lg:w-2/3 rounded-md"
+        <section className="lg:w-2/3 mb-28 rounded-md"
         style={{width:"733.33px",height:"547.89px"}}>
           <div className="bg-gray-100 shadow-sm rounded-md"
           style={{width:"717.33px",height:"62.89px",left:"8px",position:"relative",
@@ -45,12 +93,14 @@ const cart: React.FC = () => {
            <div className="main div1 rounded-md shadow-inner shadow-gray-100"
            style={{width:"717.33px",height:"436px",top:"60.89px",left:"8px",position:"relative",
             paddingTop:"24px",paddingBottom:"24px", boxShadow:"0.2%"}}>
-              
-              <div className="gap-8 mainproduct1 div flex"
+           {products.map((item)=>(
+          
+            
+              <div key={item._id} className="gap-8 p-3 mainproduct1 div flex"
               style={{width:"717.33px",height:"170px" }}>
 
                 <Image className="rounded-md" width={150} height={150} alt="image"
-                src="/boy7.jpeg"
+                src={item.image}
                 />
                 <div className="gap-6"
                 style={{width:"537.33px",height:"170px"}}>
@@ -60,14 +110,14 @@ const cart: React.FC = () => {
                   style={{width:"227px",height:"28px",top:"1.89px",left:"6px",position:"relative",
                     fontSize:"15px",fontWeight:"500",lineHeight:"28px"}}>
 
-                      Nike Dri-FIT ADV TechKnit Ultra
+                      {item.title}
                   </p>
 
                   <p className="text-gray-600"
                   style={{width:"117px",height:"28px",textAlign:"right",left:"178px",position:"relative",
                     fontSize:"15px",fontWeight:"400",lineHeight:"28px"}}>
 
-                  MRP: ₹ 3 895.00
+                  MRP: ₹ {item.price}
                   </p>
                   </div>
 
@@ -83,7 +133,7 @@ const cart: React.FC = () => {
                   style={{width:"173px",height:"28px",top:"1.89px",left:"6px",position:"relative",
                     fontSize:"15px",fontWeight:"400",lineHeight:"28px"}}>
 
-                      Ashen Slate/Cobalt Bliss
+                     Category:{item.category}
                   </p>
 
                   <div className="flex">
@@ -106,109 +156,49 @@ const cart: React.FC = () => {
                   </div>
 
                   <div className="gap-2 flex"
-                  style={{width:"537.33px",height:"64px",paddingTop:"36px",paddingRight:"473.33px",position:"relative",
-                    paddingBottom:"6px"}}>
+                  style={{width:"537.33px",height:"64px"}}>
 
-                    <Image width={24} height={24} alt="image"
-                    style={{top:"3.75px",left:"2.25px",borderWidth:"1.5px",
-                      position:"relative"}}
-                      src="/heart.png"
+                  <button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="black"
+                      strokeWidth="1"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        d="M12 21s-7-4.434-7-11a5 5 0 0 1 9-3 5 5 0 0 1 9 3c0 6.566-7 11-7 11z"
+                      />
+                    </svg>
+                  </button>
+
+                <button onClick={() => handleRemoveCart(item._id)} className="rounded hover:bg-gray-100 text-red-600">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 8.25v10.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 18.75V8.25M9.75 12v4.5m4.5-4.5v4.5M4.5 6.75h15M10.5 6.75V5.25a1.5 1.5 0 011.5-1.5h0a1.5 1.5 0 011.5 1.5v1.5m-6 0h6"
                     />
+                  </svg>
+                </button>
 
-                <Image width={24} height={24} alt="image"
-                    style={{top:"3.75px",left:"20px",borderWidth:"1.5px",
-                      position:"relative"}}
-                      src="/recyclebin.png"
-                    />
-                  </div>
-
-                </div>
-
-              </div>
-
-
-              <div className="gap-8 mainproduct2 mt-8 div flex"
-              style={{width:"717.33px",height:"170px" }}>
-
-                
-                <div className="gap-6 ml-44"
-                style={{width:"537.33px",height:"170px"}}>
-                  
-                  <div className="flex">
-                  <p
-                  style={{width:"227px",height:"28px",top:"1.89px",left:"6px",position:"relative",
-                    fontSize:"15px",fontWeight:"500",lineHeight:"28px"}}>
-
-                  Nike Air Max 97 SE
-                  </p>
-
-                  <p className="text-gray-600"
-                  style={{width:"117px",height:"28px",textAlign:"right",left:"178px",position:"relative",
-                    fontSize:"15px",fontWeight:"400",lineHeight:"28px"}}>
-
-                  MRP: ₹ 16 995.00
-                  </p>
-                  </div>
-
-
-                  <p className="text-gray-600"
-                  style={{width:"232px",height:"28px",top:"1.89px",left:"6px",position:"relative",
-                    fontSize:"15px",fontWeight:"400",lineHeight:"28px"}}>
-
-                          Men&#39;s Shoes
-                  </p>
-
-                  <p className="text-gray-600 text-nowrap"
-                  style={{width:"173px",height:"28px",top:"1.89px",left:"6px",position:"relative",
-                    fontSize:"15px",fontWeight:"400",lineHeight:"28px"}}>
-
-                  Flat Pewter/Light Bone/Black/White
-                  </p>
-
-                  <div className="flex">
-
-                  <p className="text-gray-600"
-                  style={{width:"173px",height:"28px",top:"1.89px",left:"6px",position:"relative",
-                    fontSize:"15px",fontWeight:"400",lineHeight:"28px"}}>
-
-                      Size
-                  </p>
-
-                  <p className="text-gray-600"
-                  style={{width:"173px",height:"28px",top:"1.89px",right:"120px",position:"relative",
-                    fontSize:"15px",fontWeight:"400",lineHeight:"28px"}}>L</p>
                     
-                    <p className="text-gray-600"
-                  style={{width:"173px",height:"28px",top:"1.89px",right:"240px",position:"relative",
-                    fontSize:"15px",fontWeight:"400",lineHeight:"28px"}}>Quantity<span className="mx-4">1</span></p>
-
-                  </div>
-
-                  <div className="gap-2 flex"
-                  style={{width:"537.33px",height:"64px",paddingTop:"36px",paddingRight:"473.33px",position:"relative",
-                    paddingBottom:"6px"}}>
-
-                    <Image width={24} height={24} alt="image"
-                    style={{top:"3.75px",left:"2.25px",borderWidth:"1.5px",
-                      position:"relative"}}
-                      src="/heart.png"
-                    />
-
-                <Image width={24} height={24} alt="image"
-                    style={{top:"3.75px",left:"20px",borderWidth:"1.5px",position:"relative"}}
-                      src="/recyclebin.png"
-                    />
                   </div>
 
                 </div>
 
-              </div>
+              </div> ))} 
+     
+            </div> 
 
-
-
-
-              
-            </div>      
 
           </div>
       </section>
@@ -268,12 +258,12 @@ const cart: React.FC = () => {
     </div>
         
   
-</div>
+
   
       
       
     );
   };
   
-  export default cart;
+  
   
